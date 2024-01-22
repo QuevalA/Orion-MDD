05f958911aeb91ecc8649c5e1a78ef437a6dcabb
QuevalA <87530892+QuevalA@users.noreply.github.com>
2024-01-22 17:15:19 +0100

    Create required components, add basic navigation.

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import {TopicsListComponent} from "./features/topics/topics-list/topics-list.component";
+import {PostsListComponent} from "./features/posts/posts-list/posts-list.component";
+import {UserDetailComponent} from "./features/user/user-detail/user-detail.component";
+import {PostDetailComponent} from "./features/posts/post-detail/post-detail.component";
+import {PostCreationComponent} from "./features/posts/post-creation/post-creation.component";
+import {RegisterComponent} from "./features/auth/register/register.component";
+import {LoginComponent} from "./features/auth/login/login.component";
+import {NotFoundComponent} from "./pages/not-found/not-found.component";
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  {
+  path: '',
+  component: HomeComponent
+  },
+  {
+    path: 'register',
+    component: RegisterComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'topics',
+    component: TopicsListComponent
+  },
+  {
+    path: 'posts',
+    component: PostsListComponent
+  },
+  {
+    path: 'posts/:id',
+    component: PostDetailComponent
+  },
+  {
+    path: 'posts/create',
+    component: PostCreationComponent
+  },
+  {
+    path: 'user',
+    component: UserDetailComponent
+  },
+  {
+    path: '**', // wildcard
+    component: NotFoundComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,9 +5,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PostsListComponent } from './features/posts/posts-list/posts-list.component';
+import { PostDetailComponent } from './features/posts/post-detail/post-detail.component';
+import { PostCreationComponent } from './features/posts/post-creation/post-creation.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { TopicsListComponent } from './features/topics/topics-list/topics-list.component';
+import { UserDetailComponent } from './features/user/user-detail/user-detail.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, PostsListComponent, PostDetailComponent, PostCreationComponent, RegisterComponent, LoginComponent, TopicsListComponent, UserDetailComponent, HeaderComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
